27a2f5de79baa503c9d128812f4036f4ca6c1333
Jatin Kumar <100685507+Jatin2k2@users.noreply.github.com>
2023-01-06 09:09:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let header = document.querySelector('.header-2');
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+// Deal countDown Starts
+
+let countDate = new Date('december 1, 2022 00:00:00').getTime();
+
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
+
+
+// Deal countDown Ends
+
+// slideshow starts
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// slideshow ends
+
+// Storing the data in local Storage
+
+let eStore = []
+function store(){
+   var inputEmail= document.getElementById("email");
+   eStore.push(inputEmail.value)
+   localStorage.setItem("email",JSON.stringify(eStore));
+}
